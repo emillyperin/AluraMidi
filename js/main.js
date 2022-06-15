4de04b4c9799c618cbf646f116ad97ece4aa06b3
Emily Perin <emilyperin5@gmail.com>
2022-06-14 21:08:20 -0300

    feat: automation of the sound repoduction of each button

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
+function tocaSom(idElementAudio){
+    document.querySelector(idElementAudio).play();
+};
 
-function tocaSomPom(){
-
-    document.querySelector('#som_tecla_pom').play();
-}
-
-const listaTeclas = document.querySelectorAll('.teclas');
+const listaTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 //enquanto o contador for menor que 9, que é a quantidade de teclas, ele executará o código, sempre adicionando +1 ao número do contador
-while (contador > listaTeclas.length) {
-    listaTeclas[0].onclick = tocaSomPom;
+while (contador < listaTeclas.length) {
+
+    const tecla = listaTeclas[contador];
+    const instrumento = tecla.classList[1]; //final da classe, que é igual aos IDs dos áudios
+
+    //template string -> precisa ser com crase
+    const idAudio = `#som_${instrumento}`; //início do id dos audios e personaliza conforme o final da classe de cada elemento
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    };
     contador++;
-}
+}; 
